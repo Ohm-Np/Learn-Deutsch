b221f1a0bf012959d7430dcfabe6ebaed3f341da
Ohm-Np <om.bhandari1995@gmail.com>
2025-01-24 15:55:47 +0100

    added chapters and updated interface

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 // Show an alert with a random German word
-const words = ['Apfel', 'Hund', 'Katze'];
+const easyWords = ['Apfel', 'Hund', 'Katze', 'Haus', 'Brot', 'Maus', 'Baum', 'Auto', 'Stuhl', 'Tisch'];
+const mediumWords = ['Garten', 'Schule', 'Fenster', 'Bücher', 'Erdbeere', 'Wolke', 'Blume', 'Schmetterling', 'Reise', 'Lampe'];
+const hardWords = ['Gesellschaft', 'Wirtschaft', 'Entwicklung', 'Unabhängigkeit', 'Schwierig', 'Erinnerung', 'Verantwortung', 'Einschränkung', 'Erfahrung', 'Bevölkerung'];
+
+const words = [...easyWords, ...mediumWords, ...hardWords];
+
 document.addEventListener('DOMContentLoaded', () => {
     const randomWord = words[Math.floor(Math.random() * words.length)];
     alert(`Random word: ${randomWord}`);
